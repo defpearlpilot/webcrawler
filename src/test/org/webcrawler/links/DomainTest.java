68fbfb1c7d3dddd21280f7c55fa84f04e8c5b065
Andrew Tuliszewski <defpearlpilot@yahoo.com>
2017-02-23 19:37:23 -0500

    - fix up the pom

--- src/test/org/webcrawler/links/DomainTest.java
+++ src/test/org/webcrawler/links/DomainTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
