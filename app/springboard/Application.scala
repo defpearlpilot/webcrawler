a14a3ff583617d72459d8eac2f879b05e6297663
andrew <defpearlpilot@yahoo.com>
2017-08-21 17:25:01 -0400

    additional changes to sort out actor props

--- app/springboard/Application.scala
+++ app/springboard/Application.scala
@@ -4,10 +4,12 @@ import java.net.URL
 
 import akka.actor.{ActorSystem, Props}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.util.Timeout
 import akka.stream.OverflowStrategy.dropHead
+import akka.stream.scaladsl.RunnableGraph
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 case class Message(message: String)
@@ -23,14 +25,22 @@ object Application {
 
     val url = new URL(baseUrl)
 
+    val q1 = Source.queue[Message](100, dropHead)
+
     val queue = Source.queue[Message](100, dropHead).to(Sink.foreach(println)).run()
 
+
+    val sink = Sink.fold[Message, Message](Message("hello"))((m1, m2) => m2)
 
     queue.offer(Message("this"))
     queue.offer(Message("is"))
     queue.offer(Message("a"))
     queue.offer(Message("message"))
+    queue.complete()
 
+    val flow: RunnableGraph[Future[Message]] = q1.toMat(sink)(Keep.right)
+    val eventualMessage = flow.run()
+    println(eventualMessage)
     //    val indexer = actorSystem.actorOf(Props[SiteIndexingActor](new SiteIndexingActor(url)))
 //
 //    URLSupport.toURL(baseUrl).foreach(url => indexer ! IndexURL(url))
